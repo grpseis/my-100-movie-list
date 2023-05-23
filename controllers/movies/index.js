ded868e24fb0956e423bd8b7c6992da55ad26960
Git-Delwing <delwing.macmaster@gmail.com>
2023-05-22 21:42:57 -0500

    Actualizamos proyecto

--- controllers/movies/index.js
+++ controllers/movies/index.js
@@ -7,12 +7,9 @@ router.use(authMiddleware);
 
 
 
-
+// Endpoint para consultar las listas de todos los usuarios
 router.get('/list/all', (req, res) => {
-    //req.jwt_payload.role
-    //res.send("Endpoint para consultar las listas de todos los usuarios");
-
-    moviesMethods.allMovieslist(req, res);
+     moviesMethods.allMovieslist(req, res);
 });
 
 /*router.get('/list/all', (req, res) => {
@@ -20,23 +17,29 @@ router.get('/list/all', (req, res) => {
     moviesMethods.allMovieslist(req, res);
 });
 */
+
+//Endpoint para consultar la lista de peliculas de un usuario
 router.get('/list/:id', (req, res) => {
-    res.send("Endpoint para consultar la lista de peliculas de un usuario: " + 
-    JSON.stringify(req.params));
+    moviesMethods.oneMovieslist(req , res);
 });
 
+
+//Endpoint para añadir peliculas a una lista
 router.post('/list/:id/add', (req, res) => {
-    //res.send("Endpoint para añadir peliculas a una lista: " + JSON.stringify(req.params));
     moviesMethods.registerMovie(req, res);
 });
 
+
+// Endpoint para eliminar peliculas a una lista
+// para eliminar utilizamos la posición de la pelicula en la lista
 router.delete('/list/:id/delete/:movie_id', (req, res) => {
-    res.send("Endpoint para eliminar peliculas a una lista: ", 
-    JSON.stringify(req.params));
+    //res.send("Endpoint para eliminar peliculas a una lista: " + JSON.stringify(req.params));
+    moviesMethods.deleteMovie(req, res);
 });
 
+//Endpoint para calificar listas de otros usuarios
 router.put('/list/:id/rate', (req, res) => {
-    res.send("Endpoint para calificar listas de otros usuarios");
+    moviesMethods.rateUser(req, res);
 });
 
 module.exports = router;
--- controllers/movies/methods.js
+++ controllers/movies/methods.js
@@ -1,7 +1,7 @@
 const movieList = require("../../models/movieList");
 const { json } = require("express");
 
-// Listamos todos las listas de peliculas
+//Consultamos todas las listas de los usuarios
 const allMovieslist = async (req, res) => {
     try {
         const lamov = await movieList.find();
@@ -13,58 +13,114 @@ const allMovieslist = async (req, res) => {
 }
 
 
+// Consultamos todos las peliculas de un usuario
+const oneMovieslist = async (req, res) => {
+    try {
+        const userName = req.params.id;
+        const lamov = await movieList.findOne({owner : userName});
+        if (!lamov) throw new error(`No hay listas de películas para el usuario ${userName}`);
+        res.json(lamov.movies);
+    } catch (error) {
+        res.status(500).send(`No hay listas de películas para el usuario ${userName}`);
+    }
+}
+
 //agregamos una lista de peliculas nueva
 const registerMovie = async (req, res) => {
     try {
         const userName = req.params.id;
         const { body } = req;
-       /* const saveMovies = async () => {
-            let MovieIDs = [];
-            console.log("numero de peliculas",body.movies.length);
-            for (let i = 0; i < body.movies.length; i++) {
-                const el = body.movies[i];
-                const newMovie = new movieList({
-                    name: el.name,
-                    year: el.year,
-                    image: el.image
-                });
-                const savedMovies = await newMovie.save();
-                movieIDs.push(saveMovies._id);
-            }
-            return movieIDs;
-        };
+        // verificamos que la lista de usuario ya exista
+        const lamov = await movieList.findOne({owner : userName});
+        // cargamos los nuevos datos
+       if (!lamov) {
+            const saveMovie = new movieList({
+                name: body.name,
+                owner: userName,
+                rating: body.rating,
+                //movies: movieIDs,
+                movies: body.movies
+            });
+            await saveMovie.save();
+            res.status(200).json(saveMovie);
+        } else {
+            lamov.movies= body.movies;
+            updMovie = await movieList.findOneAndUpdate({owner : userName},
+                lamov, {new: true});
+            res.status(200).send({"Msg" : `La lista de películas para el usuario ${userName} ya existe, adicionamos sólo peliculas`});
+        }
 
-        const movieIDs = await saveMovies();
-        console.log(movieIDs);  */
-
-        const saveMovie = new movieList({
-            name: body.name,
-            owner: userName,
-            rating: body.rating,
-            //movies: movieIDs,
-            movies: body.movies
-        });
-        //console.log(saveMovie);
-        //return saveMovie;
-        await saveMovie.save();
-        res.status(200).json(saveMovie);
     }catch (error) {
-        console.log(error);
         if (error.code === 11000) throw new error('No se pudo agregar la lista de películas');
         else throw error;
     }
 };
 
+/* const saveMovies = async () => {
+     let MovieIDs = [];
+     console.log("numero de peliculas",body.movies.length);
+     for (let i = 0; i < body.movies.length; i++) {
+         const el = body.movies[i];
+         const newMovie = new movieList({
+             name: el.name,
+             year: el.year,
+             image: el.image
+         });
+         const savedMovies = await newMovie.save();
+         movieIDs.push(saveMovies._id);
+     }
+     return movieIDs;
+ };
+
+ const movieIDs = await saveMovies();
+ console.log(movieIDs);  */
+
+
+
+
 
 // elimminar una película de una lista
 const deleteMovie = async (req, res) =>{
     try{
-        const fuser = await Movies.findOneAndDelete({nombre: req.body.nombre});
-        if (fuser) res.status(200).send({"Msg" : "Película eliminada exitosamente"});
-        else throw new error();
+        const userName = req.params.id;
+        const idPelicula = req.params.movie_id;
+        const lamov = await movieList.findOne({owner : userName});
+        if (!lamov)  throw new error(`No hay listas de películas para el usuario ${userName}`);
+        const pelis = lamov.movies[idPelicula];
+        lamov.movies.splice(idPelicula,1);
+        updMovie = await movieList.findOneAndUpdate({owner: userName},
+            lamov,
+            {new: true});
+        res.status(200).send({"Msg" : ` ${pelis.name}  ha sido eliminada de la lista`});
     } catch (e) {
         res.status(500).send({"Msg" : "Película no encontrada para eliminar"})
     }
 }
 
-module.exports =  { allMovieslist,  registerMovie };
+const rateUser = async (req, res) => {
+    try {
+        const lamov = await movieList.find();
+        const nroList = lamov.length
+        if (!lamov) throw new error('No hay listas de películas');
+        let sumaRate = 0;
+        for (let i = 0; i < nroList; i++) {
+             console.log(lamov.rating[i]);
+             sumaRate += lamov.rating[i];
+        }
+        console.log(sumaRate);
+        console.log(nroList);
+        let promedio = sumaRate / nroList;
+
+        console.log(promedio);
+        lamov.rating = promedio;
+        updRate = await movieList.findOneAndUpdate({owner: userName},
+            lamov,
+            {new: true});
+        res.status(200).send({"Msg" : `El rating de ${userName}  ha sido actualizado a ${promedio}`});
+    } catch (e) {
+        res.status(500).send({"Msg" : "No se pudo hacer el rating"});
+    }
+}
+
+
+module.exports =  { allMovieslist, oneMovieslist, registerMovie, deleteMovie, rateUser };
